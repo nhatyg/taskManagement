aeab457ec8f9e84143ba7ece5eb4d851f92e3ff7
nhatyg <buivietnhatyg@gmail.com>
2024-04-23 22:14:07 +0700

    update mock data

--- src/mocks/taskData.js
+++ src/mocks/taskData.js
@@ -4,28 +4,32 @@ export const taskData = [
     title: "Transfer",
     description: "Transfer spec anken 1813...",
     status: 3,
-    expiryDate: "2024-04-01"
+    startDate: "2024-03-30",
+    endDate: "2024-04-01"
   },
   {
     id: 2,
     title: "Estimate",
     description: "Estimate effort for anken 1813...",
     status: 3,
-    expiryDate: "2024-04-07"
+    startDate: "2024-04-02",
+    endDate: "2024-04-07"
   },
   {
     id: 3,
     title: "Basic Design",
     description: "Create invest for FR1...",
     status: 2,
-    expiryDate: "2024-04-14"
+    startDate: "2024-04-08",
+    endDate: "2024-04-14"
   },
   {
     id: 4,
     title: "Detail Design",
     description: "Create ERD for FR1...",
     status: 2,
-    expiryDate: "2024-04-25"
+    startDate: "2024-04-15",
+    endDate: "2024-04-25"
   },
   {
 
@@ -33,48 +37,55 @@ export const taskData = [
     title: "Coding",
     description: "Coding for get request definition API...",
     status: 1,
-    expiryDate: "2024-04-28"
+    startDate: "2024-04-26",
+    endDate: "2024-04-28"
   },
   {
     id: 6,
     title: "PCL",
     description: "Create testcase for UT manual...",
     status: 1,
-    expiryDate: "2024-02-03"
+    startDate: "2024-04-29",
+    endDate: "2024-05-03"
   },
   {
     id: 7,
     title: "UT manual",
     description: "Get EVD UT manual for anken 1813...",
     status: 1,
-    expiryDate: "2024-05-10"
+    startDate: "2024-04-04",
+    endDate: "2024-05-10"
   },
   {
     id: 8,
     title: "Testscript",
     description: "Coverage source code anken 1813...",
     status: 1,
-    expiryDate: "2024-05-17"
+    startDate: "2024-04-11",
+    endDate: "2024-05-17"
   },
   {
     id: 9,
     title: "QDT",
     description: "Create testcase for anken 1813...",
     status: 3,
-    expiryDate: "2024-04-23"
+    startDate: "2024-04-18",
+    endDate: "2024-04-23"
   },
   {
     id: 10,
     title: "CCL",
     description: "Create CCL for anken 1813...",
     status: 2,
-    expiryDate: "2024-04-30"
+    startDate: "2024-04-24",
+    endDate: "2024-04-30"
   },
   {
     id: 11,
     title: "CTE",
     description: "Confirm and get EVD testcase anken 1813...",
     status: 1,
-    expiryDate: "2024-05-17"
+    startDate: "2024-05-01",
+    endDate: "2024-05-17"
   }
 ];
